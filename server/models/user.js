14b3e0e99f2964e3c66181a6ba676e4e6f39c47f
pieroborrellidev <pieroborrellidev@gmail.com>
2018-10-23 22:49:18 +0200

    created first endpoint

---
+++ server/models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'); 
+
+var User = mongoose.model('User', {
+    email: {
+      type: String, 
+      required: true, 
+      minlenght: 1, 
+      trim: true
+    }
+  });
+  
+  module.exports = {
+    User
+};
+  
+  
